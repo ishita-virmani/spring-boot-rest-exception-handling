75a875aece594ec6cdbb37e99665577c2c5f0259
Ishita Virmani <ishita.virmani@infoedge.com>
2023-07-20 17:05:01 +0530

    Pushing all the source code

---
+++ src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -0,0 +1,40 @@
+package com.luv2code.demo.rest;
+
+import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> students;
+
+    //define @PostConstruct to load student data..only once
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
+        students.add(new Student("Ishita","Virmani"));
+        students.add(new Student("Anil","Virmani"));
+        students.add(new Student("Sunita","Virmani"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        if(studentId>=students.size() || studentId<0){
+            throw new StudentNotFoundException("Student Id not found-"+studentId);
+        }
+
+        return students.get(studentId);
+    }
+}
